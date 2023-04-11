177a8d02fe4ec06a8dd0c9c11cf11cfda1ad3916
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-11 19:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	 // Get the form inputs using their names
+  const firstName = document.forms["form1"]["fname"].value;
+  const lastName = document.forms["form1"]["lname"].value;
+
+  // Display the first and last name using alert
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 
 }
